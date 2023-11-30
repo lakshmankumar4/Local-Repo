83dd36bc821ef75682b7b99aca860aba823aa6b8
lakshmankumar4 <73119833+lakshmankumar4@users.noreply.github.com>
2023-11-30 11:40:11 +0530

    Add files via upload

---
+++ SampleProgram.java
@@ -0,0 +1,12 @@
+public class SampleProgram {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int sum = a + b;
+        int var1 = a * b;
+        int var2 = a / b;
+        int var3 = 15;
+        System.out.println("Sum: " + sum);
+    }
+}
